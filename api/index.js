c55db25280c12a47cb20ffbdf537cd210c9a5155
Peter E <peter_mcdohl@yahoo.com>
2021-11-07 08:08:48 +0700

    Add get feed endpoint
    - short polling
    - long polling

--- api/index.js
+++ api/index.js
@@ -41,6 +41,72 @@ app.post('/feed', (req, res) => {
   res.status(200).json({ message: 'OK' })
 })
 
+const pageSize = 5
+
+app.get('/feed/short/:lastIndex', (req, res) => {
+  let lastIndex = parseFloat(req.params.lastIndex)
+  if (isNaN(lastIndex)) {
+    lastIndex = -1
+  }
+
+  let result = []
+  if (lastIndex < 0) {
+    // if there is no last index then get the last 5 (page size) items
+    result = feedList.slice(-pageSize, feedList.length)
+    lastIndex = feedList.length - 1
+  } else {
+    // get next 5 items (page size)
+    result = feedList.slice(lastIndex + 1, lastIndex + pageSize)
+    if (lastIndex + pageSize < feedList.length) {
+      lastIndex += pageSize
+    } else {
+      lastIndex = feedList.length - 1
+    }
+  }
+
+  res.status(200).json({
+    data: result,
+    last_index: lastIndex
+  })
+})
+
+function sleep (ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
+
+const longPolling = async (req, res) => {
+  let lastIndex = parseFloat(req.params.lastIndex)
+  if (isNaN(lastIndex)) {
+    lastIndex = -1
+  }
+
+  let result = []
+  let startIndex = -pageSize
+  let count = 0
+  while (result.length === 0) {
+    console.log('Attempt:', count)
+    if (count > 0) {
+      await sleep(1000)
+    }
+    if (lastIndex >= 0) {
+      startIndex = lastIndex + 1
+    }
+    result = feedList.slice(startIndex, feedList.length)
+    count++
+  }
+
+  lastIndex = feedList.length - 1
+
+  res.status(200).json({
+    data: result,
+    last_index: lastIndex
+  })
+}
+
+app.get('/feed/long/:lastIndex', longPolling)
+
 module.exports = {
   path: '/api',
   handler: app
